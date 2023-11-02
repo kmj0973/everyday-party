3c63d43b3891c3c379c5b645e4f2994b5a7f4813
[SW7] 황지은 <hwangjii1006@gmail.com>
2023-11-03 03:36:35 +0900

    :sparkles: FEAT: added header
    헤더 추가

---
+++ views/MenuDetail/MenuDetail.js
@@ -0,0 +1,8 @@
+import {Header} from "../public/Header/Header.js";
+
+
+const headerRender = () => {
+    return Header();
+}
+
+headerRender();
---
+++ views/public/Header/Header.js
@@ -0,0 +1,46 @@
+export const Header = () => {
+    const headerElement = document.createElement("header");
+    headerElement.innerHTML = `
+        <div class="top_menu_wrap">
+            <div class="top_menu">
+                <img class="logo" src="../public/image/logo.png" alt="로고">
+                <ul class="user_menu">
+                    <li><a href="#">로그인</a></li>
+                    <li><a href="#">회원가입</a></li>
+                    <li>
+                        <a href="#">
+                            <iconify-icon 
+                                icon="ion:bag" 
+                                style="color: #181619;"
+                                width="22" 
+                                height="22">
+                            </iconify-icon>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <nav>
+            <div class="category_wrap">
+                <a class="all_category_icon" href="#">
+                    <iconify-icon 
+                        icon="iconoir:menu" 
+                        style="color: #181619;" 
+                        width="24" 
+                        height="24">
+                    </iconify-icon>
+                </a>
+                <ul class="category">
+                    <li><a href="#">신상품</a></li>
+                    <li><a href="#">베스트</a></li>
+                    <li><a href="#">의류</a></li>
+                    <li><a href="#">소품</a></li>
+                    <li><a href="#">풍선</a></li>
+                    <li><a href="#">장식</a></li>
+                </ul>
+            </div>
+        </nav>
+    `;
+
+    document.body.prepend(headerElement);
+}
