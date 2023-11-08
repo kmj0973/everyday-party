478eb1a53af1122cd76a319fd9b902a509c6e55e
kmj0973 <kmj0973@naver.com>
2023-11-08 17:13:41 +0900

    :bug: FIX : header icon
    헤더 아이콘 수정

--- views/public/header/login-header.js
+++ views/public/header/login-header.js
@@ -7,6 +7,11 @@ export const Header = () => {
                 <ul class="user_menu">
                     <li><a href="/login/login.html">로그인</a></li>
                     <li><a href="/auth/auth.html">회원가입</a></li>
+                    <li style="padding-top:4px">
+                        <a href="#">
+                            <iconify-icon icon="ph:user-fill" style="color: #181619;" width="22"></iconify-icon>
+                        </a>
+                    </li>
                     <li>
                         <a href="#">
                             <iconify-icon 
